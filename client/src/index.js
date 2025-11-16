80c6d1a2b1964e5568cca3bd6b2c63937aec445f
tomansvk <tomanka.filip@gmail.com>
2025-11-16 13:16:02 +0100

    pokus o publish

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,16 @@
 // src/index.js
+import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+// app bude vždy žiť v /shopping-list-app
+const basename = "/shopping-list-app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>
 );
